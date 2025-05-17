5697baba73f0965d2dc3c2ff3b5c112a036fbb99
Andres Rios <69942140+andres4715-gif@users.noreply.github.com>
2025-05-17 16:35:28 -0500

    changing spanish for English 5

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,30 +2,28 @@ import { BaseApiService } from '../../base-api.service';
 import { UserDTO } from '../dtos/user.dto';
 import { Logger } from '../utils/logger';
 
+const endpoint = '/users';
+
 export class UserService extends BaseApiService {
   /**
-   * Obtiene todos los usuarios
+   * Get all users
    */
   async getAllUsers(): Promise<UserDTO[]> {
-    Logger.info('Obteniendo todos los usuarios');
-    // Asegúrate de que este método se llame después de inicializar el servicio
-    const response = await this.http.get<any[]>('/users');
+    Logger.info('--- Getting all the users');
+    const response = await this.http.get<any[]>(endpoint);
 
-    // La clave está en la anotación de tipo explícito
     return response.map((user): UserDTO => {
-      // Forzamos el tipo de retorno para que TypeScript sepa que es UserDTO
       return UserDTO.fromPlain.call(UserDTO, user) as UserDTO;
     });
   }
 
   /**
-   * Obtiene un usuario por su ID
+   * Get the user by ID
    */
   async getUserById(id: number): Promise<UserDTO> {
-    Logger.info(`Obteniendo usuario con ID: ${id}`);
-    const response = await this.http.get<any>(`/users/${id}`);
+    Logger.info(`--- Getting the user by ID: ${id}`);
+    const response = await this.http.get<any>(`${endpoint}/${id}`);
 
-    // Forzamos el tipo de retorno
     return UserDTO.fromPlain.call(UserDTO, response) as UserDTO;
   }
 }
